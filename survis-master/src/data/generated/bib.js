a850058abcf45bfd836d4c93798a9fc620ffade2
gugu-111 <55780464+gugu-111@users.noreply.github.com>
2024-05-16 21:03:18 +0800

    Add files via upload

---
+++ survis-master/src/data/generated/bib.js
@@ -0,0 +1,32 @@
+﻿const generatedBibEntries = {
+    "Beck2016Visual": {
+        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
+        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
+        "doi": "10.1109/TVCG.2015.2467757",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
+        "number": "01",
+        "publisher": "IEEE",
+        "series": "TVCG",
+        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "type": "article",
+        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
+        "volume": "22",
+        "year": "2016"
+    },
+    "LLM": {
+        "abstract": "Text-to-Music Retrieval, finding music based on a given natural language query, plays a pivotal role in content discovery within extensive music databases. To address this challenge, prior research has predominantly focused on a joint embedding of music audio and text, utilizing it to retrieve music tracks that exactly match descriptive queries related to musical attributes (i.e. genre, instrument) and contextual elements (i.e. mood, theme). However, users also articulate a need to explore music that shares similarities with their favorite tracks or artists, such as I need a similar track to Superstition by Stevie Wonder. To address these concerns, this paper proposes an improved Text-to-Music Retrieval model, denoted as TTMR++, which utilizes rich text descriptions generated with a finetuned large language model and metadata. To accomplish this, we obtained various types of seed text from several existing music tag and caption datasets and a knowledge graph dataset of artists and tracks. The experimental results show the effectiveness of TTMR++ in comparison to state-of-the-art music-text joint embedding models through a comprehensive evaluation involving various musical text queries. ",
+        "author": "SeungHeon Doh, Minhee Lee,Dasaem Jeong,Juhan Nam",
+        "doi": "10.1109/ICASSP48485.2024.10446380",
+        "journal": " IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP)",
+        "keywords": "type:Music Informational Retrieval, Text-to-Music Retrieval, Large Language Mode",
+        "number": "01",
+        "publisher": "IEEE",
+        "series": "TVCG",
+        "title": "Enriching Music Descriptions with A Finetuned-LLM and Metadata for Text-to-Music Retrieval",
+        "type": "article",
+        "url": "https://ieeexplore.ieee.org/stamp/stamp.jsp?tp",
+        "volume": "22",
+        "year": "2024"
+    }
+};
